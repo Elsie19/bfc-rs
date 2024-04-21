aa6dfc5dc10f1b76fc4c0ebd9b074ee3a0e41a3c
Elsie <elsie19@pm.me>
2024-04-21 16:12:13 -0500

    Add `-s` for shell

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use execute::interpret::interpret;
 use execute::machine::Machine;
 use parse::ast::{balance_brackets, generate_ast};
 use parse::optimizer::{optimize, OptimizerStrategies};
+use reedline::{DefaultPrompt, EditMode, ExampleHighlighter, Reedline, Signal, StyledText};
 
 /// bfc is a brainfuck compiler/interpreter
 #[derive(Parser, Debug)]
@@ -18,25 +19,54 @@ pub struct Args {
     #[arg(short, long, conflicts_with = "compile", default_value_t = true)]
     interpret: bool,
 
+    /// Run interactive shell
+    #[arg(short, long, conflicts_with_all = ["compile", "interpret"], default_value_t = false)]
+    shell: bool,
+
     /// Compile to assembly
     #[arg(short, long, conflicts_with = "interpret")]
     compile: bool,
 
     /// Input file
-    #[clap(required = true)]
+    #[clap(required_unless_present = "shell")]
     rest: Vec<PathBuf>,
 }
 
 fn main() {
     let args = Args::parse();
-    let file_contents = fs::read_to_string(args.rest.get(0).unwrap()).expect("Could not read file");
-    let mut file_contents = file_contents.chars();
     let optimizings = vec![
         OptimizerStrategies::Contractions,
         OptimizerStrategies::ClearLoop,
         OptimizerStrategies::DeadCode,
         OptimizerStrategies::PureCode,
     ];
+
+    if args.shell {
+        let mut line_editor = Reedline::create();
+        let prompt = DefaultPrompt::new(
+            reedline::DefaultPromptSegment::Empty,
+            reedline::DefaultPromptSegment::CurrentDateTime,
+        );
+        loop {
+            let sig = line_editor.read_line(&prompt);
+            match sig {
+                Ok(Signal::Success(buffer)) => {
+                    let ast = optimize(generate_ast(&mut buffer.chars()), optimizings.to_owned());
+                    let mut machine = Machine::new(30_000);
+                    interpret(&ast, &mut machine);
+                }
+                Ok(Signal::CtrlD) | Ok(Signal::CtrlC) => {
+                    println!("\nQuitting! Hope you aren't insane yet!");
+                    std::process::exit(130);
+                }
+                x => {
+                    println!("Event: {:?}", x);
+                }
+            }
+        }
+    }
+    let file_contents = fs::read_to_string(args.rest.get(0).unwrap()).expect("Could not read file");
+    let mut file_contents = file_contents.chars();
     match balance_brackets(&file_contents) {
         Err(nar) => {
             eprintln!("{}", nar);
@@ -46,7 +76,6 @@ fn main() {
     }
     let ast = generate_ast(&mut file_contents);
     let ast = optimize(ast, optimizings);
-
     if args.compile {
         todo!("Make compiling work");
     } else if args.interpret {
