d03638a30a0c05e4b9fc221493ba1294d2b0bec2
Elsie <elsie19@pm.me>
2024-04-21 18:12:47 -0500

    Implement Display for OpCodes

--- src/parse/opcodes.rs
+++ src/parse/opcodes.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum OpCodes {
     Add(u8),
@@ -9,4 +11,40 @@ pub enum OpCodes {
     Loop(Vec<OpCodes>),
     // Special optimizations
     Clear,
+}
+
+impl fmt::Display for OpCodes {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Add(x) => {
+                write!(fmt, "{}", format!("{:+<1$}", "", usize::from(*x))).unwrap();
+            }
+            Self::Sub(x) => {
+                write!(fmt, "{}", format!("{:-<1$}", "", usize::from(*x))).unwrap();
+            }
+            Self::Inc(x) => {
+                write!(fmt, "{}", format!("{:><1$}", "", usize::from(*x))).unwrap();
+            }
+            Self::Dec(x) => {
+                write!(fmt, "{}", format!("{:<<1$}", "", usize::from(*x))).unwrap();
+            }
+            Self::Output => {
+                write!(fmt, "{}", ".").unwrap();
+            }
+            Self::Input => {
+                write!(fmt, "{}", ",").unwrap();
+            }
+            Self::Clear => {
+                write!(fmt, "{}", "[-]").unwrap();
+            }
+            Self::Loop(x) => {
+                write!(fmt, "{}", "[").unwrap();
+                for item in x {
+                    write!(fmt, "{}", item).unwrap();
+                }
+                write!(fmt, "{}", "]").unwrap();
+            }
+        }
+        Ok(())
+    }
 }
